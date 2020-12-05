063f7f1a27bf3f2d9d4a482399def23618f027fc
Azim Charaniya <azim.charaniya20@gmail.com>
2020-12-05 16:55:51 -0400

    Day1 Code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
+fun readLines(path: String): List<String> {
+    val lines = mutableListOf<String>()
+    val input = Scanner(FileInputStream(File(path).also { println("name = ${it.absolutePath}") }))
+    while (input.hasNextLine()) {
+        val num = input.nextLine()
+        if (num.isEmpty())
+            break
+        lines.add(num)
+    }
+    input.close()
+    return lines.toList()
 }
